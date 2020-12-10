817c66c893a3cd5edeeaee910e3d2be8548c92f0
hardikkamboj <hardikkamboj1@gmail.com>
2020-12-10 10:46:48 +0530

    added new changes

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,11 @@
 
 # Create your views here.
-from django.http import HttpResponse 
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-
 from django.urls import reverse
-
 from django.views import generic
+from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
